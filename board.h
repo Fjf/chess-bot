e37bc643fd1430d70c805ecfb9cb15674a1ca303
Duncan Kampert <fjf@hotmail.nl>
2020-12-12 19:52:21 +0100

    Added some defines for easy access to pieces

--- board.h
+++ board.h
@@ -1,8 +1,34 @@
 #ifndef CHESS_BOT_BOARD_H
 #define CHESS_BOT_BOARD_H
 
-struct board {
 
+// 0 - P R N B K Q
+
+
+#define W_PAWN 1
+#define W_ROOK 2
+#define W_KNIGHT 3
+#define W_BISHOP 4
+#define W_QUEEN 5
+#define W_KING 6
+
+#define B_PAWN 7
+#define B_ROOK 8
+#define B_KNIGHT 9
+#define B_BISHOP 10
+#define B_QUEEN 11
+#define B_KING 12
+
+#define W_KING_MOVED 0
+#define W_LROOK_MOVED 1
+#define W_RROOK_MOVED 2
+#define B_KING_MOVED 3
+#define B_LROOK_MOVED 4
+#define B_RROOK_MOVED 5
+
+struct board {
+    char board[8*8];
+    char has_moved[6];
 };
 
 void init_board(struct board* board);
